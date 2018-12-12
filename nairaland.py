00dd851cc85dd60ca015eb581964f38d1acac958
nnaemeka <robertnnaemeka@gmail.com>
2018-12-12 12:29:58 -0800

    Tidied up one

--- nairaland.py
+++ nairaland.py
@@ -1,6 +1,3 @@
-
-# coding: utf-8
-
 import time
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -42,77 +39,83 @@ for page in range(number_of_pages):
         else:
             #print(a['href'][:11],a['href'].split())
             link_piece = a['href'][:11]
-            add_link = "https://www.nairaland.com/1049481/how-place-targeted-ads-nairaland"
-            if link_position>=1 and link_piece == "https://www" and a['href'] != add_link: #118: #range of link that refers to posts
+            advert_link = "https://www.nairaland.com/1049481/how-place-targeted-ads-nairaland"
+        if link_position>=1 and link_piece == "https://www" and a['href'] != advert_link: #118: #range of link that refers to posts
             #if link_position>=58 and link_position<=63: #118: #range of link that refers to posts
-                print("Currently at the URL:",page+1, a['href'])
-                number_of_pages = get_number_of_pages(a['href'])
-                site = a['href']
-                df1 = []
-                time.sleep(10)
-                for i in range(number_of_pages):
-                    print("At page",i)
-                    if i == 0:
-                        url = site
-                        req = requests.get(url)
-                        soup = BeautifulSoup(req.text, 'html.parser')
-                        #gets this values from the first page since the section has page numbers
-                        #in subsequent pages
-                        text = soup.text
-                        title = soup.title.text
-                        if title == "This topic has been removed or hidden":
-                            print("No More")
-                            total_comments_by_text_slicing = number_of_comments_by_tags_extraction = 0
-                            break
-                        else:
-                            pass
-                        number_of_comments_by_tags_extraction = len(get_comments(soup))
-                        Topic = '-'.join(title.split("-")[:-2])
-                        Section = title.split("-")[-2]
-                        Country = title.split("-")[-1]
-                        Author = "Yes"
-                        Usernames,Gender,Shares,Likes,total_comments_by_text_slicing = \
-                        get_username_gender_shares_and_likes(text,Topic,number_of_comments_by_tags_extraction)
-                    else:
-                        url = site+"/"+str(i)
-                        req = requests.get(url)
-                        soup = BeautifulSoup(req.text, 'html.parser')
-                        number_of_comments_by_tags_extraction = len(get_comments(soup))
-                        text = soup.text
-                        #Author = "No"
-                        #time.sleep(10)
-                        Usernames,Gender,Shares,Likes,total_comments_by_text_slicing = \
-                        get_username_gender_shares_and_likes(text,Topic,number_of_comments_by_tags_extraction)
+            print("Currently at the URL:",page+1, a['href'])
+            number_of_pages = get_number_of_pages(a['href'])
+            site = a['href']
+            df1 = []
+            post_available = True # assume the post has not been removed
+            post_proccessed = True # assume that the post was successfully processed
+            time.sleep(10)
+            for i in range(number_of_pages):
+                print("At page",i)
+                if i == 0:
+                    url = site
+                    Author = "Yes"
+                else:
+                    url = site+"/"+str(i)
+                    Author = "No"
+                req = requests.get(url)
+                soup = BeautifulSoup(req.text, 'html.parser')
+                #gets this values from the first page since the section has page numbers
+                #in subsequent pages
+                text = soup.text
+                title = soup.title.text
+                #print(title)
+                if title == "This topic has been removed or hidden":
+                    print("Page has been removed")
+                    #total_comments_by_text_slicing = number_of_comments_by_tags_extraction = 0
+                    post_available = False
+                    break
+                else:
+                    pass
+                number_of_comments_by_tags_extraction = len(get_comments(soup))
+                Topic = '-'.join(title.split("-")[:-2])
+                Section = title.split("-")[-2]
+                Country = title.split("-")[-1]
+                Usernames,Gender,Shares,Likes,total_comments_by_text_slicing = \
+                get_username_gender_shares_and_likes(text,Topic,number_of_comments_by_tags_extraction)
                     #print("AUTHOR",Author)
-                    Comments = get_comments(soup)
-                    Date = get_date(soup)
-                    #total_comments_by_text_slicing = get_total_comment_by_text_splicing(a,topic)
-                    if number_of_comments_by_tags_extraction == total_comments_by_text_slicing: #gets good data
-                        print(number_of_comments_by_tags_extraction,total_comments_by_text_slicing,len(Date))            
-                        post_features = get_post_features(Usernames,Gender,Date,Topic,Author,Section,Country,Comments,Likes,Shares)
-                        headers = ["Username","Gender","Date","Topic","Author","Section","Country","Comments","Likes","Shares",]
-                        df3 = pd.DataFrame(post_features, columns=headers)
-                        df1.append(df3)
-                    else: #discard wrong data
-                        bad_sites.append(site)
-                        print("Bad Site!!!")
-                        print(number_of_comments_by_tags_extraction,total_comments_by_text_slicing)            
-                        continue
-                if number_of_comments_by_tags_extraction == total_comments_by_text_slicing and total_comments_by_text_slicing > 0: #    process correct data.
-                    df2 = pd.concat((df for df in df1),axis=0)
-                    processed_posts.append(df2)
-                    processed_sites.append(site)
-                    # save it
-                    save_processed_posts(processed_posts,processed_sites)
+                Comments = get_comments(soup)
+                Date = get_date(soup)
+                #total_comments_by_text_slicing = get_total_comment_by_text_splicing(a,topic)
+                if number_of_comments_by_tags_extraction == total_comments_by_text_slicing: #gets good data
+                    print(number_of_comments_by_tags_extraction,total_comments_by_text_slicing,len(Date))            
+                    post_features = get_post_features(Usernames,Gender,Date,Topic,Author,Section,Country,Comments,Likes,Shares)
+                    headers = ["Username","Gender","Date","Topic","Author","Section","Country","Comments","Likes","Shares",]
+                    df3 = pd.DataFrame(post_features, columns=headers)
+                    df1.append(df3)
+                    #print(df3)
                 else: #discard wrong data
                     bad_sites.append(site)
-                    save_bad_sites(bad_sites)
-                    print("Discarding!!!")
-                    pass
-            link_position +=1
-        
-    df = pd.concat((df for df in processed_posts),axis=0)
-    data_frame.append(df)
+                    print("Bad Site!!!")
+                    post_proccessed = False
+                    print(number_of_comments_by_tags_extraction,total_comments_by_text_slicing)            
+                    break
+            #if number_of_comments_by_tags_extraction == total_comments_by_text_slicing and total_comments_by_text_slicing > 0: #    process correct data.
+            if post_available == True and post_proccessed == True:
+                df2 = pd.concat((df for df in df1),axis=0)
+                processed_posts.append(df2)
+                #print(processed_posts)
+                processed_sites.append(site)
+                # save it
+                save_processed_posts(processed_posts,processed_sites)
+            else: # the post is not available or the post was not successfully processed
+                bad_sites.append(site)
+                save_bad_sites(bad_sites)
+                print("Discarding the link as it either unavailable or was not successfully processed")
+        else:
+            pass
+                
+        link_position +=1 
+        #print(processed_posts)
+    if len(processed_posts) >1: #check if the processed post container contain some processed post
+        df = pd.concat((df for df in processed_posts),axis=0)
+        data_frame.append(df)
+    else: #don't concatnate if it is empty. It can be empty if it is not the desired link, post has been removed or post couldn't be processed
+        pass
 df = pd.concat((d_f for d_f in data_frame),axis=0)
 df.reset_index(inplace=True)
 #df.to_csv("Nairaland.csv",index=True)
